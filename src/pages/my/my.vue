f5f2e196d4ba83e326fa5901250a299d4dd85164
Bella Zhang <bella_zhang@epam.com>
2024-01-08 15:10:07 +0800

    init

---
+++ src/pages/my/my.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+</script>
+
+<template>
+  <view class="my">
+    <view>111</view>
+    <image src="" mode="aspectFill" />
+    <view>会员信息：{{ memberStore.profile }}</view>
+    <button
+      @tap="
+        memberStore.setProfile({
+          nickname: 'znz',
+        })
+      "
+      size="mini"
+      plain
+      type="primary"
+    >
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
