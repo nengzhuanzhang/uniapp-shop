67d66b6df69bf84d880577bf27e362b627d29e74
Bella Zhang <bella_zhang@epam.com>
2024-01-25 16:57:11 +0800

    feat: Add profile page

--- src/types/member.d.ts
+++ src/types/member.d.ts
@@ -1,11 +1,38 @@
+import type { GLTFNodeNodeRaw } from 'XrFrame/loader/glTF/scenes/GLTFNodeNode'
+
+/** 封装通用信息 */
+type BaseProfile = {
+  id: number
+  avatar: string
+  account: string
+  nickname?: string
+}
+
 /**
  * 登录用户信息
  */
-export type LoginResult = {
-  account: string
-  avatar: string
-  id: number
+export type LoginResult = BaseProfile & {
   mobile: string
-  nickname: string
   token: string
+}
+
+/** 个人信息 用户详情信息 */
+export type ProfileDetail = BaseProfile & {
+  gender?: Gender
+  birthday?: string
+  fullLocation?: string
+  profession?: string
+}
+
+/** 性别 */
+export type Gender = '女' | '男'
+
+/** 个人信息 修改请求体参数 */
+export type ProfileParams = Pick<
+  ProfileDetail,
+  'nickname' | 'gender' | 'birthday' | 'profession'
+> & {
+  provinceCode?: string
+  cityCode?: string
+  countyCode?: string
 }
