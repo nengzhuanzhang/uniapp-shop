880e0120ee9afc5ac82f6929c76018d6b2aa86a6
Bella Zhang <bella_zhang@epam.com>
2024-01-30 15:18:56 +0800

    feat: Cart page

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,3 +1,4 @@
+import type { CartItem } from '@/types/cart'
 import { http } from '@/utils/http'
 /**
  * 加入购物车
@@ -6,6 +7,58 @@ export const postMemberCartApi = (data: { skuId: string; count: number }) => {
   return http({
     method: 'POST',
     url: '/member/cart',
+    data,
+  })
+}
+
+/**
+ *
+ * 获取购物车列表
+ */
+export const getMemberCartApi = () => {
+  return http<CartItem[]>({
+    method: 'GET',
+    url: '/member/cart',
+  })
+}
+
+/**
+ * 删除 购物车
+ * @param data
+ * @returns
+ */
+export const deleteMemberCartApi = (data: { ids: string[] }) => {
+  return http({
+    method: 'DELETE',
+    url: '/member/cart',
+    data,
+  })
+}
+
+/**
+ * 修改购物车单品
+ * @param skuId SKUID
+ * @param data selected 选中状态 count 商品数量
+ */
+export const updateMemberCartBySkuIdApi = (
+  skuId: string,
+  data: { selected?: boolean; count?: number },
+) => {
+  return http({
+    method: 'PUT',
+    url: `/member/cart/${skuId}`,
+    data,
+  })
+}
+
+/**
+ * 购物车全选/取消全选
+ * @param data selected 是否选中
+ */
+export const putMemberCartSelectedApi = (data: { selected: boolean }) => {
+  return http({
+    method: 'PUT',
+    url: '/member/cart/selected',
     data,
   })
 }
---
+++ src/types/cart.d.ts
@@ -0,0 +1,13 @@
+export type CartItem = {
+  id: string
+  skuId: string
+  name: string
+  picture: string
+  count: number
+  price: number
+  nowPrice: number
+  stock: number
+  selected: boolean
+  attrsText: string
+  isEffective: boolean
+}
